b088f35c9841ce5c00fb91a1f1f79c47a429d2f3
Sascha Köhne <koehne83@googlemail.com>
2020-01-09 15:52:02 +0100

    Finally MPerson for Start

--- VCore_Lib/Model/MPerson.cs
+++ VCore_Lib/Model/MPerson.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace VCore_Lib.Model
 {
-    public class MPerson : MBase, IMBase
+    public class MPerson : MBase, IMBase, IEquatable<MPerson>
     {
         private string _VName;
         private string _NName;
@@ -23,6 +23,46 @@ namespace VCore_Lib.Model
             get { return _NName; }
             set { if (_NName != value) { _NName = value; RaisePropertyChanged("NName"); } }
         }
+        public string Mid 
+        { 
+            get { return _Mid; }
+            set { if (_Mid != value) { _Mid = value; RaisePropertyChanged("Mid"); } }
+        }
+        public string TaughtNr
+        {
+            get { return _TaughtNr; }
+            set { if (_TaughtNr != value) { _TaughtNr = value; RaisePropertyChanged("TaughtNr"); } }
+        }
+        public string FullName { get { return $@"{NName}, {VName}"; } }
 
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as MPerson);
+        }
+
+        public bool Equals([AllowNull] MPerson other)
+        {
+            return other != null &&
+                   base.Equals(other) &&
+                   VName == other.VName &&
+                   NName == other.NName &&
+                   Mid == other.Mid &&
+                   TaughtNr == other.TaughtNr;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), VName, NName, Mid, TaughtNr);
+        }
+
+        public static bool operator ==(MPerson left, MPerson right)
+        {
+            return EqualityComparer<MPerson>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(MPerson left, MPerson right)
+        {
+            return !(left == right);
+        }
     }
 }
