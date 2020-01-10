59ae9ce33f65ad4d185c6710e78cab2f6fa66f3b
Sascha Köhne <koehne83@googlemail.com>
2020-01-10 18:29:34 +0100

    TabView Add Prepare Persons

---
+++ VCore_App/View/UCTabViewPersons.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VCore_App.View
+{
+    /// <summary>
+    /// Interaktionslogik für UCTabViewPersons.xaml
+    /// </summary>
+    public partial class UCTabViewPersons : UserControl
+    {
+        public UCTabViewPersons()
+        {
+            InitializeComponent();
+        }
+    }
+}
