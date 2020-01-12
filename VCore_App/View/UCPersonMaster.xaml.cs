7a817da7658afdb2d7184388214dc692305f16d3
Sascha Köhne <koehne83@googlemail.com>
2020-01-12 18:10:14 +0100

    prepare Changes for splitting TabView in singleclass,

---
+++ VCore_App/View/UCPersonMaster.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VCore_App.View
+{
+    /// <summary>
+    /// Interaktionslogik für UCPersonMaster.xaml
+    /// </summary>
+    public partial class UCPersonMaster : UserControl
+    {
+        public UCPersonMaster()
+        {
+            InitializeComponent();
+            Refresh_Click(this, null);
+        }
+
+        public void Refresh_Click(object sender, RoutedEventArgs e) {
+            //this.DataContext = new ViewModel.VMPerson();
+        }
+    }
+}
---
+++ VCore_App/View/UCStundenMaster.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VCore_App.View
+{
+    /// <summary>
+    /// Interaktionslogik für UCStundenMaster.xaml
+    /// </summary>
+    public partial class UCStundenMaster : UserControl
+    {
+        private string _UserId;
+        public string UserId { get { return _UserId; } set { _UserId = value; } }
+        public UCStundenMaster()
+        {
+            InitializeComponent();
+            UserId = "All";
+            //this.DataContext = new ViewModel.VMStunden(UserId);
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            //this.DataContext = new ViewModel.VMStunden(UserId);
+        }
+    }
+}
